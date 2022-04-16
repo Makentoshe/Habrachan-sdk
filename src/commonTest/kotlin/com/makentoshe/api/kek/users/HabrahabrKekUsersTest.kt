1ba5831125b13524c3ba410b505e3b9697329162
Makentoshe <mkliverout@gmail.com>
2022-04-16 17:53:54 +0300

    test: implement common tests
    Common tests just checks urls, queries, headers and other requireable stuff

--- src/commonTest/kotlin/com/makentoshe/api/kek/users/HabrahabrKekUsersTest.kt
+++ src/commonTest/kotlin/com/makentoshe/api/kek/users/HabrahabrKekUsersTest.kt
@@ -1,52 +1,45 @@
-package com.makentoshe.habrachan.api.kek.users
+package com.makentoshe.api.kek.users
 
 import com.makentoshe.habrachan.api.Habrahabr
+import com.makentoshe.habrachan.api.common.ApiRequestMethod
 import com.makentoshe.habrachan.api.common.build
-import com.makentoshe.habrachan.api.execute
 import com.makentoshe.habrachan.api.kek.kek
 import com.makentoshe.habrachan.api.kek.users.filter.UserSubscriptionsFilter
+import com.makentoshe.habrachan.api.kek.users.subscriptions
+import com.makentoshe.habrachan.api.kek.users.user
+import com.makentoshe.habrachan.api.kek.users.users
+import com.makentoshe.habrachan.api.kek.users.whois
 import com.makentoshe.habrachan.entity.userlogin
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.http.HttpStatusCode
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlinx.coroutines.runBlocking
 
 class HabrahabrKekUsersTest {
 
     private val gateway = Habrahabr.kek()
-    private val httpClient = HttpClient(CIO)
 
     /** WhoIs contains main user information like */
     @Test
-    fun `test should check get user whois info available`(): Unit = runBlocking {
+    fun testShouldCheckGetUserWhoisInfoAvailable() {
         val request = gateway.users().user(userlogin("Makentoshe")).whois().build()
         assertEquals("https://habr.com/kek/v2/users/Makentoshe/whois", request.url)
-
-        val response = request.execute(httpClient)
-        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(ApiRequestMethod.Get, request.method)
     }
 
     @Test
-    fun `test should check get user hubs info available`(): Unit = runBlocking {
+    fun testShouldCheckGetUserHubsInfoAvailable() {
         val request = gateway.users().user(userlogin("Makentoshe")).subscriptions {
             this.sort = UserSubscriptionsFilter.Sort.Hubs
         }.build()
         assertEquals("https://habr.com/kek/v2/users/Makentoshe/subscriptions/hubs", request.url)
-
-        val response = request.execute(httpClient)
-        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(ApiRequestMethod.Get, request.method)
     }
 
     @Test
-    fun `test should check get user companies info available`(): Unit = runBlocking {
+    fun testShouldCheckGetUserCompaniesInfoAvailable() {
         val request = gateway.users().user(userlogin("Makentoshe")).subscriptions {
             this.sort = UserSubscriptionsFilter.Sort.Companies
         }.build()
         assertEquals("https://habr.com/kek/v2/users/Makentoshe/subscriptions/companies", request.url)
-
-        val response = request.execute(httpClient)
-        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(ApiRequestMethod.Get, request.method)
     }
 }
